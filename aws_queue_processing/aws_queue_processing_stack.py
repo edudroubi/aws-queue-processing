d4e8ac17037416ce49bcb4d6a276802d9e187426
droubi <eduardodroubi@gmail.com>
2024-07-23 22:29:11 -0300

    create sqs queue, sns topic and lambda function to process this queue

--- aws_queue_processing/aws_queue_processing_stack.py
+++ aws_queue_processing/aws_queue_processing_stack.py
@@ -1,7 +1,11 @@
 from aws_cdk import (
-    # Duration,
+    Duration,
     Stack,
-    # aws_sqs as sqs,
+    aws_sqs as sqs,
+    aws_sns as sns,
+    aws_sns_subscriptions as subscriptions,
+    aws_lambda as _lambda,
+    aws_lambda_event_sources as lambda_event_sources
 )
 from constructs import Construct
 
@@ -10,10 +14,44 @@ class AwsQueueProcessingStack(Stack):
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
-        # The code that defines your stack goes here
+        # Create DLQ to handle with errors processing message
+        dlq = sqs.Queue(
+            self,
+            'NewQueueDLQ'
+        )
 
-        # example resource
-        # queue = sqs.Queue(
-        #     self, "AwsQueueProcessingQueue",
-        #     visibility_timeout=Duration.seconds(300),
-        # )
+        # Create SQS
+        new_queue = sqs.Queue(
+            self,
+            'NewQueue',
+            # Attach dlq to handle with errors
+            dead_letter_queue=sqs.DeadLetterQueue(
+                max_receive_count=123,
+                queue=dlq
+            )
+        )
+
+        # Create SNS topic
+        new_topic = sns.Topic(
+            self,
+            'NewTopic'
+        )
+
+        new_topic.add_subscription(subscriptions.SqsSubscription(new_queue))
+
+        # Create lambda function (this will be our topic consumer)
+        fn = _lambda.Function(
+            scope=self,
+            id='SqsProcessor',
+            runtime=_lambda.Runtime.PYTHON_3_9,
+            handler='lambda_handler.handler',
+            code=_lambda.Code.from_asset('aws_queue_processing/lambda'),
+            timeout=Duration.seconds(15),
+            memory_size=128,
+        )
+
+        # Create event source from new queue to be attached to lambda function consumer
+        event_source = lambda_event_sources.SqsEventSource(new_queue)
+
+        # Attach lambda to event source
+        fn.add_event_source(event_source)
---
+++ aws_queue_processing/lambda/lambda_handler.py
@@ -0,0 +1,4 @@
+def handler(event, lambda_context):
+    print(f'event: {event}')
+    print(f'context: {lambda_context}')
+
